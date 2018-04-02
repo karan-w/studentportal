9d99b27bdc949c701240cd5564e19d4c72e310d1
Karan Wadhwani <wadhwanikkaran@gmail.com>
2018-04-02 22:35:39 +0530

    Updated Post model

--- feed/models.py
+++ feed/models.py
@@ -6,23 +6,23 @@ def get_post_image_path(instance,filename):
 	return 'posts/{0}/{1}'.format(instance.posted_by.name,filename)
 
 class Post(models.Model):
-    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(upload_to=get_post_image_path, null=True, blank=True)
-    ACADEMIC = 'AC'
-    CLUBS = 'CL'
-    ASSOCIATIONS = 'AS'
-    CATEGORY_CHOICES = (
-        (ACADEMIC, 'Academics'),
-        (CLUBS, 'Clubs'),
-        (ASSOCIATIONS, 'Associations'),
-    )
-    category = models.CharField(
-        max_length=2,
-        choices=CATEGORY_CHOICES,
-        default=ACADEMIC,
-    )
+    # ACADEMIC = 'AC'
+    # CLUBS = 'CL'
+    # ASSOCIATIONS = 'AS'
+    # CATEGORY_CHOICES = (
+    #     (ACADEMIC, 'Academics'),
+    #     (CLUBS, 'Clubs'),
+    #     (ASSOCIATIONS, 'Associations'),
+    # )
+    # category = models.CharField(
+    #     max_length=2,
+    #     choices=CATEGORY_CHOICES,
+    #     default=ACADEMIC,
+    # )
 
     def __str__(self):
         return self.title
--- feed/views.py
+++ feed/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import *
+import datetime
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
 
-# Create your views here.
 
 # @login_required
 def feed(request):
     return render(request, 'feed/feed.html', {})
 
+# @login_required
+def addPost(request):
+    if request.method == "POST":
+        post = Post();
+        post.text = request.POST['post']
+        post.published_date = timezone.now()
+        post.save()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
